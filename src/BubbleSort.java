91ab88678d0e485eb5669ae47cdc663cb3bba08e
Chung <Joschungcarlos95@gmail.com>
2023-04-10 13:42:44 +0700

    done bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,23 @@
+
+public class BubbleSort {
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+    public static void bubbleSort(int[] list) {
+        for (int k = 1; k < list.length ; k++) {
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    /* Swap list[i] with list[i + 1] */
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+}
